8c5d870d29583ecd7bc118c7877cfaffcb44909f
karma <karmatashi954@gmail.com>
2022-06-02 10:07:04 +0545

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by karma on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
