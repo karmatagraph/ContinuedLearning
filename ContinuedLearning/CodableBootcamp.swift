7b46704f3465022107a95c1093afbfa0b0d40927
karma <karmatashi954@gmail.com>
2022-06-06 11:37:18 +0545

    add of codable without initializers

--- ContinuedLearning/CodableBootcamp.swift
+++ ContinuedLearning/CodableBootcamp.swift
@@ -13,32 +13,32 @@ struct CustomerModel: Codable, Identifiable {
     let points: Int
     let isPremium: Bool
     
-    enum CodinKeys: String, CodingKey {
-        case id, name, points, isPremium
-    }
-    
-    init(id: String, name: String, points: Int, isPremium: Bool) {
-        self.id = id
-        self.name = name
-        self.points = points
-        self.isPremium = isPremium
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodinKeys.self)
-        self.id = try container.decode(String.self, forKey: .id)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.points = try container.decode(Int.self, forKey: .points)
-        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodinKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(points, forKey: .points)
-        try container.encode(isPremium, forKey: .isPremium)
-    }
+//    enum CodinKeys: String, CodingKey {
+//        case id, name, points, isPremium
+//    }
+//    
+//    init(id: String, name: String, points: Int, isPremium: Bool) {
+//        self.id = id
+//        self.name = name
+//        self.points = points
+//        self.isPremium = isPremium
+//    }
+//    
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodinKeys.self)
+//        self.id = try container.decode(String.self, forKey: .id)
+//        self.name = try container.decode(String.self, forKey: .name)
+//        self.points = try container.decode(Int.self, forKey: .points)
+//        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
+//    }
+//    
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodinKeys.self)
+//        try container.encode(id, forKey: .id)
+//        try container.encode(name, forKey: .name)
+//        try container.encode(points, forKey: .points)
+//        try container.encode(isPremium, forKey: .isPremium)
+//    }
     
 }
 
